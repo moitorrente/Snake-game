25bc4773afb20d29e1041f6d61ac96ca0b0f6b06
moitorrente <moi.torrente@gmail.com>
2020-04-04 01:28:46 +0200

    Added game

--- sketch.js
+++ sketch.js
@@ -1,40 +1,74 @@
 const sections = 20;
 const width = 600;
 const height = 600;
-const scale = width/sections;
+const scale = width / sections;
 let snake;
+let apple;
 
 
 function setup() {
     canvas = createCanvas(width, height);
     snake = new Snake();
+    apple = new Apple();
     frameRate(10);
-    //apple = new Apple();
+    let options = {
+        preventDefault: true
+    };
+    let hammer = new Hammer(document.body, options);
+    hammer.get('swipe').set({
+        direction: Hammer.DIRECTION_ALL
+    });
+    hammer.on("swipe", swiped);
 }
 
 function draw() {
-    background(0);
-    stroke(255);
-    for(let i = 0; i< sections; i++){
+    background(60);
+    stroke(120);
+    for (let i = 0; i < sections; i++) {
         line(i * scale, 0, i * scale, height);
         line(0, i * scale, height, i * scale);
     }
+    if (snake.die()) {
+        alert('Has perdido');
+        snake.size = 0;
+    }
+    apple.show();
 
-    snake.show();
     snake.update();
+    snake.show();
+    if (snake.eat(apple)) {
+        apple.reappear();
+    }
+
+
 }
 
-function keyPressed(){
-    if(keyCode === UP_ARROW){
-        snake.setDirection(0, -1);
-    }
-    if(keyCode === DOWN_ARROW){
-        snake.setDirection(0, 1);
-    }
-    if(keyCode === RIGHT_ARROW){
+function keyPressed() {
+    if (keyCode === RIGHT_ARROW && snake.xspeed != -1) {
         snake.setDirection(1, 0);
     }
-    if(keyCode === LEFT_ARROW){
+    if (keyCode === UP_ARROW && snake.yspeed != 1) {
+        snake.setDirection(0, -1);
+    }
+    if (keyCode === DOWN_ARROW && snake.yspeed != -1) {
+        snake.setDirection(0, 1);
+    }
+    if (keyCode === LEFT_ARROW && snake.xspeed != 1) {
+        snake.setDirection(-1, 0);
+    }
+    if (key === 'A') {
+        snake.addTail();
+    }
+}
+
+function swiped(event) {
+    if (event.direction == 4 && snake.xspeed != -1) {
+        snake.setDirection(1, 0);
+    } else if (event.direction == 8 && snake.yspeed != 1) {
+        snake.setDirection(0, -1);
+    } else if (event.direction == 16 && snake.yspeed != -1) {
+        snake.setDirection(0, 1);
+    } else if (event.direction == 2 && snake.xspeed != 1) {
         snake.setDirection(-1, 0);
     }
 }
